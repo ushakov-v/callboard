c862f52c781e06b2ff6a998ea0df8985fd207d34
Vitaliy Ushakov <vitaliy-ushak0v@ya.ru>
2025-08-28 14:01:13 +0300

    Project structure changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainPage} from '../pages/main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'main', component: MainPage},
+];
